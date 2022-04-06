387a34c5a29d1854e111a6078abe5e9e4b88454e
levxxvi <99051920+levxxvi@users.noreply.github.com>
2022-04-06 14:21:54 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    /*
+    recursive fibonacci
+    fib(1) == 1
+    fib(2) == 2 || fib(2) = fib(1) + fib(1)
+
+    let n (input) = number of terms
+    let sum (output) = terms total
+    n = 5
+    fib(5) = 1 + 1 + 2 + 3 + 5
+    sum = 12 = 5 + fib(4)
+    fib(4) =
+
+    n > 2
+    fib(5) = fib(4) + fib(3)
+    fib(5) = [fib(3) + fib(2)] + [fib(2) + fib(1)]
+    fib(5) = fib(2) + fib(1) + fib(2) + fib(2) + fib(1)
+    fib(5) = 8
+
+    fib(3) = fib(3-1) + fib(3-2)
+    fib(3) = fib(2) + fib(1)
+    fib(3) = fib(
+     */
+
+    public static void main(String[] args) {
+        System.out.println(fib(9));
+    }
+
+    public static int fib(int num){
+        if(num == 1){
+            return num;
+        } else if(num == 2){
+            return num;
+        }
+        return fib(num - 1) + fib(num - 2);
+    }
+}
